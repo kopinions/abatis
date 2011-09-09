0aa92b73b86a05ebced46865e6dec94d58069f5d
WonHyeongju <wonhyeongju@gmail.com>
2011-09-09 09:36:18 +0000

    ・SQLiteOpenHelper#getWritableDatabaseにsynchronizedが宣言されていたため他の重複個所となるところからsynchronizedを削除
    ・照会クエリ結果がヌル場合、結果マップもヌルを返すように変更
    
    ・実行クエリ結果でSQLエラーの場合実行結果result行数を0にして返すように変更 
    
    ・api lv 3も対応（1.5）
    
    ーバージョン　0.2.0 リリース

--- abatis/src/asia/sonix/android/orm/OrmTestActivity.java
+++ abatis/src/asia/sonix/android/orm/OrmTestActivity.java
@@ -1,7 +1,32 @@
 package asia.sonix.android.orm;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 
 public class OrmTestActivity extends Activity {
-
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        // アクティビティの基本処理
+        super.onCreate(savedInstanceState);
+        
+        AbatisService abatisService = AbatisService.getInstance(this);
+        for (int i = 0; i < 150; i++) {
+            Map<String, Object> param = new HashMap<String, Object>();
+            param.put("orm_id", i);
+            param.put("orm_name", "won" + i);
+            param.put("orm_age", i);
+            param.put("orm_memo", "memo" + i);
+            int result = abatisService.execute("sqlId002", param);
+            Log.d("test", "result : " + result);
+        } 
+        
+        Map<String, Object> param = new HashMap<String, Object>();
+        param.put("orm_age", 28);
+        List<Map<String, Object>> result = abatisService.executeForMapList("sqlId004", param);
+        Log.d("test", "get result : " + result.size());
+    }
 }
