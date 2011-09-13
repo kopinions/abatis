5687e61b6f62094cac20deba84359bf8952c804b
WonHyeongju@gmail.com <WonHyeongju@gmail.com@43a864e7-ccba-334d-758d-18035a1df4f0>
2011-09-13 05:15:58 +0000

    ・Value Objectを用いるInterfaceを追加
    ーバージョン　0.3.0 リリース

---
+++ trunk/abatis/src/asia/sonix/android/orm/TestBean.java
@@ -0,0 +1,37 @@
+package asia.sonix.android.orm;
+
+public class TestBean {
+    
+    private int orm_id;
+    
+    private String orm_name;
+    
+    private int orm_age;
+    
+    private String orm_memo;
+    
+    public int getOrm_id() {
+        return orm_id;
+    }
+    public void setOrm_id(int orm_id) {
+        this.orm_id = orm_id;
+    }
+    public String getOrm_name() {
+        return orm_name;
+    }
+    public void setOrm_name(String orm_name) {
+        this.orm_name = orm_name;
+    }
+    public int getOrm_age() {
+        return orm_age;
+    }
+    public void setOrm_age(int orm_age) {
+        this.orm_age = orm_age;
+    }
+    public String getOrm_memo() {
+        return orm_memo;
+    }
+    public void setOrm_memo(String orm_memo) {
+        this.orm_memo = orm_memo;
+    }
+}
